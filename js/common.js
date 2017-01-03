28c426f268df0711277c4253f9e8cbc9413f6406
Ivan Anufriev <ivananufriew@gmail.com>
2017-01-03 20:57:51 +0200

    redesign

---
+++ js/common.js
@@ -0,0 +1,20 @@
+$(function() {
+
+	$('.portfolio_items').owlCarousel({
+		items	: 1,
+		autoWidth: true,
+		itemsDesktop: [1280, 1],
+		itemsDesktopSmall: [1024, 1],
+		itemsTablet: [992, 1],
+		itemsTabletSmall: [768, 1],
+		itemsMobile: [480, 1],
+		navigation	: true,
+		navigationText : "",
+		navSpeed : true,
+		center : false,
+		responsive : false,
+		loop			 : true
+	});
+
+
+});
